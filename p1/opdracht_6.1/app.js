ec9581818736ba63fa7dafcb33e5c0fa1f7b917f
Liangjiang1 <9020598@student.zadkine.nl>
2022-10-06 13:13:44 +0200

    java

--- p1/opdracht_6.1/app.js
+++ p1/opdracht_6.1/app.js
@@ -2,7 +2,8 @@
 
 function kleur_aanpassen(){
 
-document.getElementById("h1")
+document.getElementById("h1").style.color ="red";
+document.getElementById("h1").innertext = "ikke ikke";
 
 kleur_aanpassen();
 }
