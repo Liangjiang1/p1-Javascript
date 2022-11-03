c00e0f10123528bc758029ae600fc4b72428f1ce
Liangjiang1 <9020598@student.zadkine.nl>
2022-11-03 13:36:59 +0100

    Ik ben bijna nerd

---
+++ p1/opdracht-7.1/app.js
@@ -0,0 +1,18 @@
+let budget = 100;
+
+let product = 60;
+
+let h1 = document.getElementById("uitkomst");
+
+if (budget > product){ 
+console.log("ik heb genoeg geld");
+h1.innerText="ik heb genoeg geld"
+h1.style.color = "green";
+} else{
+console.log("ik heb niet genoeg geld");
+h1.innerText="ik heb niet genoeg geld"
+h1.style.color = "red";
+}
+
+
+
