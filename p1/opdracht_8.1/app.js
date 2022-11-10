1ff6d98a89573f268a31e547247834fb1718cbb8
Liangjiang1 <9020598@student.zadkine.nl>
2022-11-10 15:15:53 +0100

    8.2

--- p1/opdracht_8.1/app.js
+++ p1/opdracht_8.1/app.js
@@ -1,29 +1,44 @@
 
- let input1 = document.getElementById("input1")
- let input2 = document.getElementById("input2")
- let title = document.getElementById("title")
- 
- 
- 
- function devide(){
-    let answer = input1.value / input2.value
-    title.innerText = answer;
+let input1 = document.getElementById("input1")
+let input2 = document.getElementById("input2")
+let title = document.getElementById("title")
+
+
+
+function devide(){
+    if(input1.value <1 || input2.value <1){
+    title.innertext ="het getal is te laag"
+    } else { 
+     let answer = input1.value / input2.value
+  title.innerText= answer;}
+
+
 }
 
 function multiply(){
-    let answer = input1.value * input2.value
-    title.innerText = answer;
-    
+if (input1.value < 1 || input2.value < 1) {
+        title.innerText = "het getal is te laag";}
+    else {
+
+        let answer = input1.value * input2.value
+    title.innerText = answer;}
+
 }
 
 function add(){
+    if (input1.value < 1 || input2.value < 1){
+        title.innerText = "het getal is te laag";} 
+     else {
     let answer = Number(input1.value) + Number(input2.value)
-    title.innerText = answer;
+    title.innerText = answer;}
 
-}
+     }
 
-function minus(){
+     function minus(){
+    if (input1.value < 1 || input2.value < 1) 
+        title.innerText = "het getal is te laag";
+    
+    
     let answer = input1.value - input2.value
-    title.innerText = answer; 
-
-}
+    title.innerText = answer;
+    }
